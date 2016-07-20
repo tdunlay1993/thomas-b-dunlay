49ca45d5e7464bccb46ee6a6a2c0902bf2eecda9
Thomas Dunlay <tomdunlay1@gmail.com>
2016-07-20 12:59:28 -0600

    Committed Milestone 1 to github

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+
+		<title>My.Web.Page</title>
+	</head>
+	<body>
+
+		<header>
+			<h1>PWP Milestone 1</h1>
+		</header>
+
+		<main>
+			<h2>Purpose, Audience, Goal</h2>
+
+			<ul>
+				<li>Purpose - My purpose is showcase my development in my career</li>
+				<li>Audience - Potential Employers, Collaborators, Customers, and Clients</li>
+				<li>Goal - Create a gallery to showcase my work as a music producer and developer</li>
+			</ul>
+
+		<br><h2>Persona</h2>
+
+			<ol>
+				<li>Name - Greg Davis</li>
+				<li>Age - 40</li>
+				<li>Job - Senior Web Developer at Native Instruments</li>
+				<li>Technology - PHP, Javascript</li>
+				<li>Goal - Looking for Web Development Intern </li>
+			</ol>
+
+		<br><h2>Use Case</h2>
+
+			<ol>
+				<li>Greg Davis is looking for a web development intern. He sees that
+				Thomas Dunlay has applied. He looks at Thomas Dunlays application and
+				sees his personal site listed. Greg clicks on the link. He looks
+				through his website and learns more about whether Tom is the
+				right fit or not.</li>
+			</ol>
+
+		</main>
+		<footer>
+		</footer>
+	</body>
+</html>
